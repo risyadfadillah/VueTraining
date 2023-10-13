979293ba3099616248c3c50a591e1388d4283c77
risyad_f <risyad.fadillah@photon.com>
2023-10-13 14:21:21 +0700

    create first repo

---
+++ src/components/atoms/Header.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "Header",
+    props: {
+        title: {
+            default: "Header",
+            type: String
+        }
+    }
+
+
+}
+</script>
+
+<template>
+    <p>{{ title }}</p>
+</template>
+
+<style>
+</style>
